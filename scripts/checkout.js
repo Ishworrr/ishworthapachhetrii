4efad6fbc066265f0ebc94f1d11b620e2616b301
ishwor <ishwo5678@gmail.com>
2024-02-19 18:14:53 +1100

    save data and generate HTML for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,18 @@
 import {cart, removefromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
- 
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+hello();
+import {deliveryOptions} from '../data/deliveryOptions.js';
+
+const today = dayjs();
+const deliveryDate=today.add(7,'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
+
+
+
+
 let cartSummaryHTML = '';
 
 
@@ -10,6 +21,8 @@ const productId = cartItem.productId;
 
 let matchingProduct;
 
+
+
 products.forEach((product) => {
 if(product.id === productId){
     matchingProduct = product;
@@ -17,6 +30,20 @@ if(product.id === productId){
 }
 
 });
+
+const deliveryOptionId = cartItem.deliveryOptionId;
+
+let deliveryOption;
+
+deliveryOptions.forEach((option)=>{
+  if(option.id === deliveryOptionId){
+    deliveryOption = option;
+  }
+});
+const today= dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays,'days');
+    const dateString = deliveryDate.format('ddd,MMMM D');
+
 cartSummaryHTML += 
 
     `
@@ -54,51 +81,46 @@ cartSummaryHTML +=
         <div class="delivery-options-title">
           Choose a delivery option:
         </div>
-        <div class="delivery-option">
-          <input type="radio" checked
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Tuesday, June 21
-            </div>
-            <div class="delivery-option-price">
-              FREE Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Wednesday, June 15
-            </div>
-            <div class="delivery-option-price">
-              $4.99 - Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Monday, June 13
-            </div>
-            <div class="delivery-option-price">
-              $9.99 - Shipping
-            </div>
-          </div>
-        </div>
+        ${deliveryOptionsHTML(matchingProduct, cartItem)}
       </div>
     </div>
   </div>
     `;
 
 });
+function deliveryOptionsHTML(matchingProduct, cartItem){
+  let html = '';
+  deliveryOptions.forEach((deliveryOption)=>{
+    const today= dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays,'days');
+    const dateString = deliveryDate.format('ddd,MMMM D');
+    const priceString = deliveryOption.priceCents === 0
+        ? 'FREE' //if price cents is 0 its free.
+        : `$${formatCurrency(deliveryOption.priceCents)} -`; // we can omit -(dash) here for 4.99 -shipping and put dash here  ${priceString} - Shipping 
+
+        const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+        html +=
+
+
+   ` <div class="delivery-option">
+    <input type="radio"
+    ${isChecked ? 'checked' : ''}  <!-- just put checked  if the current product matches the one in the cart -->
+      class="delivery-option-input"
+      name="delivery-option-${matchingProduct.id}">
+    <div>
+      <div class="delivery-option-date">
+      ${dateString}
+      </div>
+      <div class="delivery-option-price">
+        ${priceString} Shipping 
+      </div>
+    </div>
+  </div>
+`
+  });
+  return html;
+}
+
 document.querySelector('.js-oder-summary')
   .innerHTML = cartSummaryHTML;
 
