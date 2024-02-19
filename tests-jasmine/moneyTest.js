60163c29680fcdfe5a99ec9f7b84d2b462e25965
ishwor <ishwo5678@gmail.com>
2024-02-20 10:08:58 +1100

    remove dfault test and add money.js test

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('testsuite: formatCurrency', ()=>{
+    it('converts cents to dollars', ()=>{
+expect(formatCurrency(2095)) 
+            .toBe('20.95');
+    });
+
+    it('works with zero',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('nearest cents',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
