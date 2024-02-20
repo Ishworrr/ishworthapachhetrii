491faa35933bcf31b9e5b0a37da340e77af8732a
ishwor <ishwo5678@gmail.com>
2024-02-20 13:52:09 +1100

    create automated tests for order summary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -28,6 +28,7 @@ it('adds new product to cart',()=>{
     });
  
     loadFromStorage();
+    
 addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 expect(cart.length).toEqual(1);
 expect(localStorage.setItem).toHaveBeenCalledTimes(1);
